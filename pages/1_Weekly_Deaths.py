8d1e437ddacb476acedfd8c45b340d578be7e838
Darragh McConville <dmc1899@googlemail.com>
2023-06-29 13:35:59 +0100

    Add metrics for data up to and including week 24 in 2023. (#46)

--- pages/1_Weekly_Deaths.py
+++ pages/1_Weekly_Deaths.py
@@ -1,14 +1,17 @@
 # ref - https://github.com/jkanner/streamlit-dataview/blob/master/app.py
-import math
 
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
 from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator, MultipleLocator
 
 st.set_page_config(layout='wide')
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-st.title('Weekly Deaths (2023 YTD)')
+st.title('Weekly Death Registrations')
 st.caption('👈 Use the sidebar to configure parameters for your analysis.')
 
 
@@ -30,9 +33,9 @@ def get_deaths_for(df, week, column):
 
 def is_higher_or_lower(value):
     if value < 0:
-        return "lower than"
+        return "lower"
     elif value > 0:
-        return "higher than"
+        return "higher"
     else:
         return "(the same as)"
 
@@ -50,33 +53,29 @@ def calculate_percentage_change(first_value, second_value):
 all_weekly_deaths_df = load_data()
 csv = convert_df(all_weekly_deaths_df)
 
-label_five_year_average_2015_to_2019 = '2015 - 2019'
-label_five_year_average_2016_to_2020 = '2016 - 2020'
-label_five_year_average_2017_to_2021 = '2017 - 2021'
-label_five_year_average_2018_to_2022 = '2018 - 2022'
-label_five_year_average_2016_to_2019_and_2021 = '2016 - 2019 and 2021'
-
-# Default UI settings
-analysis_end_week_selected = 23
-mean_value_selected = label_five_year_average_2015_to_2019
+label_five_year_average_2015_to_2019 = '2015-2019'
+label_five_year_average_2016_to_2020 = '2016-2020'
+label_five_year_average_2017_to_2021 = '2017-2021'
+label_five_year_average_2018_to_2022 = '2018-2022'
+label_five_year_average_2016_to_2019_and_2021 = '2016-2019 and 2021'
 
 with st.sidebar:
-
     st.markdown("### Configure week and average")
 
     analysis_end_week_selected = st.number_input('2023 Registration Week:', min_value=1, max_value=24, step=1, value=24,
                                                  help='The week in the current year up to which points are plotted.')
 
     mean_value_selected = st.radio(
-        "Five-year Average:",
+        "Five-year average:",
         (label_five_year_average_2015_to_2019, label_five_year_average_2016_to_2020,
          label_five_year_average_2017_to_2021, label_five_year_average_2018_to_2022,
          label_five_year_average_2016_to_2019_and_2021),
-        index=0, help='Institutions such as NISRA or ONS use different 5-year averages for comparison. '\
-                      'Select the average to include the plot.')
+        index=0,
+        help='Institutions such as NISRA or ONS use different 5-year averages for comparison. '
+             'Select the average to include the plot.')
 
     st.markdown("### Access underlying data")
-    show_raw_data_selected = st.checkbox('Show raw data', value= False, help='Display raw data below the plot.')
+    show_raw_data_selected = st.checkbox('Show raw data', value=False, help='Display raw data below the plot.')
 
     st.download_button(
         label="Download as CSV",
@@ -86,59 +85,86 @@ with st.sidebar:
         help='Download the raw data as a CSV file.'
     )
 
-if mean_value_selected == label_five_year_average_2015_to_2019:
-    mean_value_to_plot = '2015_to_2019_Mean'
-elif mean_value_selected == label_five_year_average_2016_to_2020:
-    mean_value_to_plot = '2016_to_2020_Mean'
-elif mean_value_selected == label_five_year_average_2017_to_2021:
-    mean_value_to_plot = '2017_to_2021_Mean'
-elif mean_value_selected == label_five_year_average_2018_to_2022:
-    mean_value_to_plot = '2018_to_2022_Mean'
-elif mean_value_selected == label_five_year_average_2016_to_2019_and_2021:
-    mean_value_to_plot = '2016_to_2019_and_2021_Mean'
+mapping = {
+    label_five_year_average_2015_to_2019: '2015_to_2019_Mean',
+    label_five_year_average_2016_to_2020: '2016_to_2020_Mean',
+    label_five_year_average_2017_to_2021: '2017_to_2021_Mean',
+    label_five_year_average_2018_to_2022: '2018_to_2022_Mean',
+    label_five_year_average_2016_to_2019_and_2021: '2016_to_2019_and_2021_Mean'
+}
 
+mean_value_to_plot = mapping.get(mean_value_selected, '2015_to_2019_Mean')
 mean_value_selected = mean_value_selected + ' 5yr average'
-
-st.markdown(f'''
-### Analysis for Registration Week {analysis_end_week_selected} 2023
-''')
 
 comparison_years = ['2022', '2021', '2020', mean_value_to_plot]
 this_week_2023 = get_deaths_for(all_weekly_deaths_df, analysis_end_week_selected, '2023')
 
-# Initialize a dictionary to store the results
-results = {}
+st.metric(f"Week {analysis_end_week_selected} 2023",
+          f"{this_week_2023} Deaths",
+          "",
+          delta_color="inverse",
+          help=f"There were {this_week_2023} deaths registered during registration week {analysis_end_week_selected} of 2023.")
 
-# Loop through each year and calculate the percentage change compared to this_week_2023
+st.markdown('---')
+st.markdown("#### Prior Year Comparisons")
+col2, col3, col4, col5 = st.columns(4)
+
+metric_results = {}
 for year in comparison_years:
+
     weekly_deaths = get_deaths_for(all_weekly_deaths_df, analysis_end_week_selected, year)
     percentage_change = calculate_percentage_change(weekly_deaths, this_week_2023)
-    results[f'this_week_{year}_vs_2023'] = percentage_change
-
-#streamlit.write(results)
-
-# Store the results in a list of tuples for easier iteration
-result_tuples = [
-    ('2022', results['this_week_2022_vs_2023']),
-    ('2021', results['this_week_2021_vs_2023']),
-    ('2020', results['this_week_2020_vs_2023']),
-    (f'{mean_value_selected}', results[f'this_week_{mean_value_to_plot}_vs_2023'])
-]
-
-st.markdown(f'Registration Week **{analysis_end_week_selected} in 2023** had **{this_week_2023}** registered deaths which is:')
-
-# Iterate through the results and print them out
-for year, percentage_change in result_tuples:
     percentage_change_abs = abs(percentage_change)
     comparison = is_higher_or_lower(percentage_change)
-    colour = 'red' if 'higher' in comparison else 'black'
-    st.markdown(f'- :{colour}[{percentage_change_abs}% {comparison} week {analysis_end_week_selected} in {year}]')
 
-years = [mean_value_to_plot, '2020', '2021', '2022', '2023']
+    if 'higher' in comparison:
+        colour = 'red'
+    elif 'lower' in comparison:
+        colour = 'green'
+    else:
+        colour = 'black'
+
+    if 'Mean' in year:
+        formatted_year = mean_value_selected
+        metric_title = f"Week {analysis_end_week_selected} {mean_value_selected}"
+    else:
+        formatted_year = year
+        metric_title = f"Week {analysis_end_week_selected} {year}"
+
+    metric_summary_text = f':{colour}[Week {analysis_end_week_selected} is {percentage_change_abs}% {comparison} in 2023 than in {formatted_year}.]'
+
+    metric_results[f'this_week_{year}_vs_2023'] = (metric_title, weekly_deaths, percentage_change, metric_summary_text)
+
+comparison_years = ["2022", "2021", "2020", f"{mean_value_to_plot}"]
+average_comparison_key = f'this_week_{mean_value_to_plot}_vs_2023'
+
+for year in comparison_years:
+    computed_key_value = f"this_week_{year}_vs_2023"
+    metric_title = metric_results[f"this_week_{year}_vs_2023"][0]
+    weekly_deaths = metric_results[f"this_week_{year}_vs_2023"][1]
+    percentage_change = metric_results[f"this_week_{year}_vs_2023"][2]
+
+    col = None
+    help_text = f"There were {weekly_deaths} deaths registered during registration week {analysis_end_week_selected} of {year}."
+
+    if year == f"{mean_value_to_plot}":
+        col = col5
+        average_comparison_key = f"this_week_{mean_value_to_plot}_vs_2023"
+    elif year == "2022":
+        col = col2
+    elif year == "2021":
+        col = col3
+    elif year == "2020":
+        col = col4
+
+    col.metric(metric_title, weekly_deaths, f"{percentage_change}%", delta_color="inverse", help=help_text)
+    col.markdown(metric_results[f"{computed_key_value}"][3])
+
+plot_years = [mean_value_to_plot, '2020', '2021', '2022', '2023']
 
 fig_deaths_trends = make_subplots(specs=[[{'secondary_y': False}]])
 
-for year in years:
+for year in plot_years:
     fig_deaths_trends.add_trace(
         go.Scatter(x=all_weekly_deaths_df['Registration_Week'],
                    y=all_weekly_deaths_df[year].loc[0:(analysis_end_week_selected - 1)] if year == '2023' else all_weekly_deaths_df[year],
@@ -165,18 +191,17 @@ layout = go.Layout(
 
 fig_deaths_trends = go.Figure(data=fig_deaths_trends.data, layout=layout)
 
+st.markdown('---')
 st.plotly_chart(fig_deaths_trends, use_container_width=True, theme=None)
 
 all_weekly_deaths_df_copy = all_weekly_deaths_df.copy()
 all_weekly_deaths_df_copy['week_name'] = all_weekly_deaths_df_copy.index
 
-import matplotlib.pyplot as plt
-from matplotlib.ticker import AutoMinorLocator, MultipleLocator
 
 fig, axs = plt.subplots(1, 1, constrained_layout=True, figsize=(10,4))
 
 years_to_compare = ['2023']
-index=0
+index = 0
 
 for year in years_to_compare:
 
@@ -212,6 +237,7 @@ for year in years_to_compare:
 
     index += 1
 
+st.markdown('---')
 st.pyplot(fig)
 
 if show_raw_data_selected:
